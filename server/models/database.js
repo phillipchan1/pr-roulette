6d84d7997046798a8fb2afaeef0c3ca9eba463ec
Phillip_Chan <phillip.chan@liferay.COM>
2016-11-21 13:02:16 -0800

    Changed to mLab Database

--- server/models/database.js
+++ server/models/database.js
@@ -2,9 +2,9 @@
 
 var mongoose = require('mongoose');
 var localDB = 'mongodb://localhost/pr-roulette';
-var mLabDB = 'mongodb://admin:thebible@ds049180.mlab.com:49180/matchify-dev';
+var mLabDB = 'mongodb://admin:prroulette@ds159497.mlab.com:59497/prroulette';
 
-mongoose.connect(localDB, function(err) {
+mongoose.connect(mLabDB, function(err) {
 	if (err) {
 		console.log("Failed to connect to mongodb");
 	} else {
