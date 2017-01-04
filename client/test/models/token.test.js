7ea775c8911a8736d2ee80839bab353dee1710b2
Phillip_Chan <phillip.chan@liferay.COM>
2017-01-04 10:29:35 -0800

    Set up mocha, chai as testing frameworks and created sample tests

---
+++ client/test/models/token.test.js
@@ -0,0 +1,11 @@
+describe('tokenService', function() {
+
+	it('should be true', function() {
+		true.should.be.true;
+	});
+
+	it('should be false', function() {
+		true.should.be.true;
+	});
+
+});
