25c18cc803819677b61abd4185c9eab57562abec
Phillip_Chan <phillip.chan@liferay.COM>
2017-01-05 14:51:04 -0800

    Altered karma config for Travis CI

--- my.conf.js
+++ my.conf.js
@@ -2,72 +2,82 @@
 // Generated on Wed Jan 04 2017 09:31:17 GMT-0800 (PST)
 
 module.exports = function(config) {
-  config.set({
+	var configuration = {
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+		// base path that will be used to resolve all patterns (eg. files, exclude)
+		basePath: '',
 
-    plugins : [
-        'karma-mocha',
-        'karma-chrome-launcher',
-        'karma-chai',
-        'karma-mocha-reporter'
-    ],
+		plugins: [
+			'karma-mocha',
+			'karma-chrome-launcher',
+			'karma-chai',
+			'karma-mocha-reporter'
+		],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: ['mocha', 'chai'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'client/js/app.js',
-      'client/tests/spec/**/*.js'
-    ],
+		// list of files / patterns to load in the browser
+		files: [
+			'client/js/app.js',
+			'client/tests/spec/**/*.js'
+		],
 
-    // list of files to exclude
-    exclude: [
-    ],
+		customLaunchers: {
+			Chrome_travis_ci: {
+				base: 'Chrome',
+				flags: ['--no-sandbox']
+			}
+		},
 
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+		// list of files to exclude
+		exclude: [],
 
 
-    // web server port
-    port: 9876,
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {},
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['mocha'],
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+		// web server port
+		port: 9876,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
+
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
+
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: ['Chrome'],
+
+
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: false,
+
+		// Concurrency level
+		// how many browser should be started simultaneous
+		concurrency: Infinity
+	}
+
+	if (process.env.TRAVIS) {
+		configuration.browsers = ['Chrome_travis_ci'];
+	}
+	config.set(configuration)
 }
